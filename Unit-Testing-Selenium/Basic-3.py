d1c16fa8ed4f75b6b8bf7cea2d2a0245467ee299
Hardik Rana <hardikrana276@gmail.com>
2019-09-23 19:36:34 +0530

    More files added

---
+++ Unit-Testing-Selenium/Basic-3.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome(executable_path="D:\python programs\Driver for selenium\chromedriver.exe")
+
+driver.get("http://demo.automationtesting.in/Windows.html")
+
+print(driver.title)
+
+print(driver.current_url)
+
+driver.find_element_by_xpath("//*[@id='Tabbed']/a/button").click()
+
+time.sleep(5)
+
+#driver.close() #close only currently focused tab
+
+driver.quit() #closes all tabs
+
+
+
